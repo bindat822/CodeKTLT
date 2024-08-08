9a8114f83b638069dc678894a20f32938c2b92eb
bindat822 <bindat668@gmail.com>
2024-08-09 00:12:30 +0700

    Viết hàm đổi những kí tự đầu tiên của mỗi từ thành chữ in hoa và những từ không phải đầu câu sang chữ thường

---
+++ Bai3.4.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+
+void inHoa(char* str) {
+    int i = 0;
+    bool newWord = true;
+
+    while (str[i]) {
+        if (isspace(str[i])) {
+            newWord = true;
+        }
+        else if (newWord) {
+            str[i] = toupper(str[i]);
+            newWord = false;
+        }
+        else {
+            str[i] = tolower(str[i]);
+        }
+        i++;
+    }
+}
+
+int main() {
+    char input[100];
+    printf("Nhập chuỗi: ");
+    fgets(input, 100, stdin);
+
+    // Đổi ký tự đầu của mỗi từ thành in hoa
+    inHoa(input);
+    printf("Chuỗi sau khi chuẩn hóa: \"%s\"\n", input);
+
+    return 0;
+}
