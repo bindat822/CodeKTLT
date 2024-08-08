8d5e898d9c9317390aa4baf8251b145dcd7990e0
bindat822 <bindat668@gmail.com>
2024-08-09 00:05:24 +0700

    Bai 3.3

---
+++ Bai3.3.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void xoaKhoangTrang(char* str) {
+    int i = 0, j = 0;
+    bool spaceFound = false;
+
+    while (str[i]) {
+        if (str[i] != ' ') {
+            if (j != 0 && spaceFound) {
+                str[j++] = ' ';
+            }
+            str[j++] = str[i];
+            spaceFound = false;
+        }
+        else {
+            spaceFound = true;
+        }
+        i++;
+    }
+    str[j] = '\0';
+}
+
+int main() {
+    char input[100];
+    printf("Nhập chuỗi: ");
+    fgets(input, 100, stdin);
+
+    // Xóa khoảng trắng thừa
+    xoaKhoangTrang(input);
+    printf("Chuỗi sau khi xóa khoảng trắng thừa: \"%s\"\n", input);
+
+    return 0;
+}
