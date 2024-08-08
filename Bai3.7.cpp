333ffb593adca74150c95e5f40fe49a2261ca8b2
bindat822 <bindat668@gmail.com>
2024-08-09 00:48:02 +0700

    chèn 1 từ ở vị trí

---
+++ Bai3.7.cpp
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void chenKyTu(char* str, const char* word, int position) {
+    char buffer[200];
+    int length = strlen(str);
+
+    if (position > length) {
+        position = length;
+    }
+
+    strncpy(buffer, str, position);
+    buffer[position] = '\0';
+    strcat(buffer, word);
+    strcat(buffer, str + position);
+
+    strcpy(str, buffer);
+}
+
+int main() {
+    char str[200];
+    char word[50];
+    int position;
+
+    printf("Nhập chuỗi: ");
+    fgets(str, 200, stdin);
+
+    printf("Nhập từ cần chèn: ");
+    fgets(word, 50, stdin);
+
+    printf("Nhập vị trí chèn: ");
+    scanf("%d", &position);
+
+    // Xóa ký tự newline
+    str[strcspn(str, "\n")] = '\0';
+    word[strcspn(word, "\n")] = '\0';
+
+    chenKyTu(str, word, position);
+
+    printf("Chuỗi sau khi chèn: \"%s\"\n", str);
+
+    return 0;
+}
