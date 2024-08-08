49d9d5051995bef9473711fbea7cadd2a0fd2498
bindat822 <bindat668@gmail.com>
2024-08-08 23:55:56 +0700

    Bai 3.2

---
+++ Bai3.2.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int countSpaces(char* str) {
+    int count = 0;
+    for (int i = 0; str[i]; i++) {
+        if (str[i] == ' ') {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    char input[] = "Huynh Tan Dat";
+    int numSpaces = countSpaces(input);
+    printf("Số khoảng trắng: %d\n", numSpaces);
+    return 0;
+}
