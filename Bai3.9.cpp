fcf6bbe7ff1a5e3b728f9fa643e05a0b16fac6dd
bindat822 <bindat668@gmail.com>
2024-08-09 00:59:00 +0700

    liệt kê mỗi ký tự xuất hiện mấy lần

---
+++ Bai3.9.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void lietKeSoLanXuatHien(const char* str) {
+    int freq[256] = { 0 };
+
+    while (*str) {
+        freq[(unsigned char)(*str)]++;
+        str++;
+    }
+
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] > 0) {
+            printf("Ký tự '%c' xuất hiện %d lần.\n", i, freq[i]);
+        }
+    }
+}
+
+int main() {
+    char str[100];
+
+    printf("Nhập chuỗi: ");
+    fgets(str, 100, stdin);
+
+    // Xóa ký tự newline
+    str[strcspn(str, "\n")] = '\0';
+
+    lietKeSoLanXuatHien(str);
+
+    return 0;
+}
